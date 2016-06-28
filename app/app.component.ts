63d3548c7b291971bf3ea03f820562bf6b880194
Marty Zhou <martyzhou@Objectivasoftware.com>
2016-06-28 11:35:17 +0800

    chapter 5

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -10,14 +13,7 @@ import {Component} from '@angular/core';
     <span class="badge">{{hero.id}}</span> {{hero.name}}
   </li>
 </ul>
-<div *ngIf="selectedHero">
-<h2>{{selectedHero.name}} details!</h2>
-<div><label>id: </label>{{selectedHero.id}}</div>
-<div>
-  <label>name: </label>
-  <input [(ngModel)]="selectedHero.name" placeholder="name">
-</div>
-</div>
+<my-hero-detail [hero]="selectedHero"></my-hero-detail>
 `,
     styles: [`
   .selected {
@@ -67,11 +63,21 @@ import {Component} from '@angular/core';
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 
 })
-export class AppComponent {
-    public heroes = HEROES;
+export class AppComponent implements OnInit {
+
+    constructor(private heroService: HeroService){
+    }
+
+    ngOnInit(){
+	this.getHeroes();
+    }
+    
+    public heroes: Hero[];
 
     title = 'Tour of Heroes';
 
@@ -81,22 +87,7 @@ export class AppComponent {
         this.selectedHero = hero;
     }
 
+    getHeroes(){
+	this.heroService.getHeroes().then((heroes) => this.heroes = heroes);
+    }
 }
-
-export class Hero {
-    id: number;
-    name: string;
-}
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
