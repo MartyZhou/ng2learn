075f31f789125e999cbbf0d0ec606def668fca51
Marty Zhou <martyzhou@outlook.com>
2016-07-04 23:47:31 +0800

    Use form-horizontal

--- app/hero.ts
+++ app/hero.ts
@@ -1,4 +1,12 @@
-export class Hero {
+export interface Hero {
     id: number;
     name: string;
+    power?: string[];
+    birthday?: Date;
+    height?: number;
+    email?: string;
+    country?: string;
+    color?: string;
+    img?: string;
+    point?: number;
 }
